90f908fe99132b014bffab032640dc29ab56367f
Jamie Lewis <Jamie@Jamies-MacBook-Air.local>
2023-10-08 15:49:30 +0700

    adds footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,42 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> j.h.lewis89@gmail.com</p>
+        <p>
+            <b class="pr-2">GitHub</b>
+            <a
+                href="https://github.com/JamieLewis748"
+                class="text-violet-400"
+                target="_blank"
+            >
+                jamie_lewis<sup
+                    ><span class="text-xs scale-75 pl-0.5">
+                        <i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        />
+                    </span></sup
+                >
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">LinkedIn</b>
+            <a
+                href="www.linkedin.com/in/jamielewis748"
+                class="text-violet-400"
+                target="_blank"
+            >
+                jamie_lewis<sup
+                    ><span class="text-xs scale-75 pl-0.5">
+                        <i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        />
+                    </span></sup
+                >
+            </a>
+        </p>
+    </div>
+</footer>
